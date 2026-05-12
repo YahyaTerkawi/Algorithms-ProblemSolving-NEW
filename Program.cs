ed021457b37a20ea6c546f0f0f1085f734b0f7eb
Yahya Terkawi <yahyaterkawi02@gmail.com>
2026-05-12 17:00:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿class program
+{
+
+    static void swap(ref int num1, ref int num2)
+    {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    static void SelectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int MinIndex = i;
+
+            for(int j = i; j<arr.Length;j++)
+            {
+                if (arr[j]< arr[MinIndex])
+                    MinIndex = j;
+            }
+
+            swap(ref arr[MinIndex],ref arr[i]);
+        
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = {4,6,7,8,324,65,88,0,-1 };
+
+        SelectionSort(arr);
+
+        foreach (int num in arr)
+        {
+            Console.Write($"   {num}");
+        }
+
+        Console.ReadKey();
+    }
+}
